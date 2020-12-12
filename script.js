6f75740e2f3ac8b542ff31e4dc2ead3e23de473e
Wess A <Wess A>
2020-12-12 09:58:23 -0800

    working clock

---
+++ script.js
@@ -0,0 +1,54 @@
+// Clock
+var tickingClock = setInterval(clock, 1000);
+function clock () {
+  var myDate = new Date ();
+  var clock = myDate.toLocaleTimeString();
+  document.getElementsByClassName("clock").innerHTML = clock;
+};
+
+//Clock Styles
+var clockColor = document.getElementsByClass("clock");
+document.setAttribute("style","color: yellow;");
+
+//Pacific Time Styles
+var redType = document.getElementById("red-type");
+document.setAttribute("style", "color: red;");
+
+
+
+/*
+Broken Down
+
+var tickingClock = setInterval(clock, 1000);
+// We are setting a variable that makes the clock tick using setInterval
+
+setInterval Syntax
+var intervalId = scope.setInterval(func, [delay, arg1, arg2, ...]);
+
+
+function clock () {
+// creating a funtion of the clock.
+
+  var myDate = new Date ();
+  //new Date is used to work with dates and in this clock will be referenced by .toLocaleTimeString().
+  new Date carries the year, month, day, hours, minuttes, seconds, milliseconds information at the time it is called. (setInterval is calling new Date every second to get a ticking clock)
+
+
+    var clock = myDate.toLocaleTimeString();
+    //.toLocaleTimeString method returns ONLY the time section of  new Date()
+    -->ONLY the time section of new Date()
+        const event = new Date(`December 9,2020 11:21:00 GMT+00:00);
+          console.log(event.toLocaleTimeString('en-US));
+          11:21:00 = ONLY the time section of new Date()
+
+
+    document.getElementById("clock").innerHTML = clock;
+};.
+//document --> Traversing the DOM
+document is the highest OBJECT in the document tree. It is the root object.
+
+.getElementById() is connecting the id instife the () to the html document
+
+.innerHTML returns, sets, or changes the HTML content of an element. Here it's returning the content in the variable "clock" to the html document.
+
+*/
