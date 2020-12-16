d7fc2487f74d463e0486a30b08e1d3438094795b
froggy <chrisdetmering@gmail.com>
2020-12-16 11:44:03 -0800

    Refactor complete

--- script.js
+++ script.js
@@ -1,55 +1,42 @@
-// Clock
-var tickingClock = setInterval(clock, 0);
+clock()
+setInterval(clock, 1000);
 function clock () {
-  var myDate = new Date ();
-  var clock = myDate.toLocaleTimeString();
-  document.getElementById("clock").innerHTML = clock;
+  let myDate = new Date ();
+  let militaryHours = myDate.getHours(); 
+  let standardHours = toStandardHours(militaryHours);
+  let minutes = addLeadingZero(myDate.getMinutes()); 
+  let seconds = addLeadingZero(myDate.getSeconds());
+
+  document.getElementById("clock")
+  .textContent = `${standardHours}:${minutes}:${seconds} ${getAmPm(militaryHours)}`;
 };
 
-//Clock Styles
-var clockColor = document.getElementsById("clock");
-clockColor.style.color = "yellow";
-clockColor.style.top = "10px";
 
-//Pacific Time Styles
-var redType = document.getElementById("red-type");
-redType.style.color= "red";
+function addLeadingZero(time) {  
+  if (time < 10) { 
+    return `0${time}`; 
+  }
+  return time; 
+}
 
-
-
-/*
-Code Broken Down
-
-var tickingClock = setInterval(clock, 0);
-// We are setting a variable that makes the clock tick using setInterval. The delay is set to zero so the clock will load with the page and continually display the time.
-
-setInterval Syntax
-var intervalId = scope.setInterval(func, [delay, arg1, arg2, ...]);
+function getAmPm(hr) { 
+  if (hr < 12) { 
+    return 'AM'; 
+  }
+  return 'PM'; 
+}
 
 
-function clock () {
-// creating a funtion of the clock.
+function toStandardHours(hr) {
+  if (hr === 0) { 
+    return 12; 
+  }
 
-  var myDate = new Date ();
-  //new Date is used to work with dates and in this clock will be referenced by .toLocaleTimeString().
-  new Date carries the year, month, day, hours, minuttes, seconds, milliseconds information at the time it is called. (setInterval is calling new Date every second to get a ticking clock)
+  if (hr < 13) { 
+    return hr; 
+  }
 
-
-    var clock = myDate.toLocaleTimeString();
-    //.toLocaleTimeString method returns ONLY the time section of  new Date()
-    -->ONLY the time section of new Date()
-        const event = new Date(`December 9,2020 11:21:00 GMT+00:00);
-          console.log(event.toLocaleTimeString('en-US));
-          11:21:00 = ONLY the time section of new Date()
-
-
-    document.getElementById("clock").innerHTML = clock;
-};.
-//document --> Traversing the DOM
-document is the highest OBJECT in the document tree. It is the root object.
-
-.getElementById() is connecting the id instife the () to the html document
-
-.innerHTML returns, sets, or changes the HTML content of an element. Here it's returning the content in the variable "clock" to the html document.
-
-*/
+  if (hr >= 13) { 
+    return hr % 12; 
+  }
+}
